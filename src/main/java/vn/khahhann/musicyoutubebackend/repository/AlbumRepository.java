bb0c4cdd83fa98413e06b28ff7b53c182e3a68f0
khahhan <khanhanbui1010@gmail.com>
2023-10-24 11:59:26 +0700

    add find album

--- src/main/java/vn/khahhann/musicyoutubebackend/repository/AlbumRepository.java
+++ src/main/java/vn/khahhann/musicyoutubebackend/repository/AlbumRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import vn.khahhann.musicyoutubebackend.entity.Album;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "albums")
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
+    List<Album> findByIsRecapTrue();
+    List<Album> findByIsRadioTrue();
+    List<Album> findByIsAlbumTrue();
+    List<Album> findByIsPlaylistTrue();
+
+
 }
